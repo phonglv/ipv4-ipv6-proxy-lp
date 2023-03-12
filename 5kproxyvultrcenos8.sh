e55c42915d8b2a655bcf6c08bb06481d97c3406f
phonglv <levanphong1993@gmail.com>
2023-03-12 18:05:39 +0700

    Update 5kproxyvultrcenos8.sh
    newalinh

--- 5kproxyvultrcenos8.sh
+++ 5kproxyvultrcenos8.sh
@@ -75,13 +75,11 @@ EOF
 
 upload_proxy() {
     cd $WORKDIR
-    local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -F "file=@proxy.zip" https://file.io)
+    zip proxy.zip proxy.txt
+    URL=$(curl -F "file=@proxy.zip" https://beta.apileak.com/api/proxy --ssl-no-revoke)
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
-    echo "Password: ${PASS}"
+    echo "Check imported at: https://beta.apileak.com/manage/proxy"
 
 }
 gen_data() {
