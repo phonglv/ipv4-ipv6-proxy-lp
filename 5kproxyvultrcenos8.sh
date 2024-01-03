35d2be52698f997409196f956002187527bc3a0f
phonglv <levanphong1993@gmail.com>
2024-01-03 16:55:00 +0700

    update update file

--- 5kproxyvultrcenos8.sh
+++ 5kproxyvultrcenos8.sh
@@ -77,7 +77,7 @@ upload_proxy() {
     cd $WORKDIR
    # URL=$(curl -F "file=@${IP4}.txt" https://beta.apileak.com/api/proxy)
 local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
+    zip --password $PASS proxy.zip ${IP4}.txt
     URL=$(curl -s --upload-file ./proxy.zip https://transfer.whalebone.io/yanproxy.zip)
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
