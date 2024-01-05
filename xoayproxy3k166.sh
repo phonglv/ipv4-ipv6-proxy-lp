829882272807edfb54a0d2deecb7016570be2d6e
phonglv <levanphong1993@gmail.com>
2024-01-05 11:58:04 +0700

    Update xoayproxy3k166.sh

--- xoayproxy3k166.sh
+++ xoayproxy3k166.sh
@@ -115,7 +115,7 @@ echo "installing apps"
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir -y $WORKDIR && cd $_
 
 #IP4=$(curl -4 -s icanhazip.com)
 IP4="192.168.1.166"
