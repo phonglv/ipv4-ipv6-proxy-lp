508a902ddd48bcd8b23e4d97b9671c03b2a91271
phonglv <levanphong1993@gmail.com>
2024-01-05 13:11:37 +0700

    Update xoayproxy3k166.sh

--- xoayproxy3k166.sh
+++ xoayproxy3k166.sh
@@ -130,7 +130,7 @@ echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
 FIRST_PORT=10000
 #LAST_PORT=$(($FIRST_PORT + $COUNT))
-LAST_PORT=10010
+LAST_PORT=10002
 echo "gen_data"
 gen_data >$WORKDIR/data.txt
 echo "gen_iptables"
