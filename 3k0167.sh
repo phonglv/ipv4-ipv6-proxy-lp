c07a1dc56ff87e19efb0de4b071142a089df6603
phonglv <levanphong1993@gmail.com>
2024-01-05 16:17:19 +0700

    Update 3k0167.sh 198.128

--- 3k0167.sh
+++ 3k0167.sh
@@ -118,7 +118,7 @@ WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
 #IP4=$(curl -4 -s icanhazip.com)
-IP4="192.168.0.167"
+IP4="192.168.198.128"
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
