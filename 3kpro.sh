ba07dc20e8da67b9b54ee1353e7108f1c6219d21
phonglv <levanphong1993@gmail.com>
2024-01-13 23:22:00 +0700

    Update user pass 3kpro.sh

--- 3kpro.sh
+++ 3kpro.sh
@@ -79,7 +79,8 @@ upload_proxy() {
 local PASS=$(random)
     zip --password $PASS proxy.zip ${IP4}.txt
     # URL=$(curl -s --upload-file ./proxy.zip https://transfer.whalebone.io/yanproxy.zip)
-	URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+    URL=$(curl -s --upload-file proxy.zip https://file.io)
+	#URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
     echo "Password: ${PASS}"
@@ -90,7 +91,7 @@ local PASS=$(random)
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
        # echo "$(random)/$(random)/$IP4/$port/$(gen64 $IP6)"
-       echo "usr/pass/$IP4/$port/$(gen64 $IP6)"
+       echo "lephong/lvphong1/$IP4/$port/$(gen64 $IP6)"
 	#echo "lephong/lephong1234/$IP4/$port/$(gen64 $IP6)"
     done
 }
